6d452dabacf927793819118605fc1338c6aa4950
Tatayecorp <julien.vancutsem@gmail.com>
2017-05-31 15:03:04 +0200

    Add files via upload

---
+++ chars/readme.txt
@@ -0,0 +1,2 @@
+Put your characters in this directory.
+Edit data/select.def to add characters to the selection list.
---
+++ js/player.js
@@ -0,0 +1,36 @@
+
+function player ( res ) {
+			
+	this.pos = { x : 0, y : 0 };
+	this.action = 0;
+	this.currentFrame = 0;
+	this.currentTime = 0;
+	this.right = 1;
+	this.palette = null;
+	
+	var hasOwn = Object.prototype.hasOwnProperty;
+	if ( typeof res != 'object' ) {
+		throw TypeError( 'player - ressource incorrect' );
+	}
+	var properties = Object( res );
+	for ( var prop in properties ) {
+		if ( hasOwn.call( properties, prop ) ) {
+			this[ prop ] = properties[ prop ];
+		}
+	}
+	
+}
+
+player.prototype = {
+	indexOf : function( groupNumber, imageNumber ) {
+		if ( this.SFF == null ) {
+			throw new TypeError( 'indexOf - SFF not defined.' );
+		}
+		for ( var i = 0; i < this.SFF.images.length; i++ ) {
+			if ( this.SFF.images[ i ].groupNumber === groupNumber && this.SFF.images[ i ].imageNumber === imageNumber ) {
+				return i;
+			}
+		}
+		return -1;
+	}
+}

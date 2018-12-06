4d3d7240e256df18d6ba8c545d42e67353cddd58
Carlson Santana Cruz <carlsonsantana@gmail.com>
2018-12-06 00:09:52 -0300

    :recycle: Create MugenJSApp class to execute functions of app.js

--- src/app.js
+++ src/app.js
@@ -11,159 +11,168 @@ var requestAnimFrame = (function() {
         };
 })();
 
-// Create the canvas
-function createCanvas(canvasWidth, canvasHeight, zoom) {
-    var canvas = document.createElement('canvas');
-    canvas.width = canvasWidth * zoom;
-    canvas.height = canvasHeight * zoom;
-    document.body.appendChild(canvas);
-    return canvas.getContext('2d');
-}
-
-// The main game loop
-var lastTime;
-var fps;
-function main(ctx, player1, player2, canvasWidth, canvasHeight) {
-    var now = Date.now();
-    var dt = (now - lastTime) / 1000.0;
-    fps = Math.ceil(1000 / (now - lastTime));
-    update();
-    render(ctx, player1, player2, canvasWidth, canvasHeight);
-
-    lastTime = now;
-    requestAnimFrame(function() {
-        main(ctx, player1, player2, canvasWidth, canvasHeight);
-    });
-}
-
-function loadCharacters(characters, callback) {
-    var resources = [];
-
-    for (var i = 0, length = characters.length; i < length; i++) {
-        var character = characters[i];
-
-        resources.push(new resource.Resource(character.path, character.name));
+class MugenJSApp {
+    constructor(player1, player2, canvasWidth, canvasHeight, zoom) {
+        this.player1 = player1;
+        this.player2 = player2;
+        this.canvasWidth = canvasWidth;
+        this.canvasHeight = canvasHeight;
+        this.zoom = zoom;
     }
 
-    Promise.all(resources.map(function(resource) {
-        return resource.load();
-    })).then(function() {
-        callback.call(this, resources);
-    });
-}
+    static loadCharacters(characters, callback) {
+        var resources = [];
 
-function init(player1, player2, canvasWidth, canvasHeight, zoom) {
-    var ctx = createCanvas(canvasWidth, canvasHeight, zoom);
-    reset(ctx, zoom);
-    lastTime = Date.now();
-    main(ctx, player1, player2, canvasWidth, canvasHeight);
-}
+        for (var i = 0, length = characters.length; i < length; i++) {
+            var character = characters[i];
 
-// Game state
-var gameTime = 0;
-var isGameOver;
-var score;
+            resources.push(
+                new resource.Resource(character.path, character.name)
+            );
+        }
 
-// Update game objects
-function update(dt) {
-    //handleInput(dt);
-    //checkCollisions();
-}
+        Promise.all(resources.map(function(resource) {
+            return resource.load();
+        })).then(function() {
+            callback.call(this, resources);
+        });
+    }
 
-// Draw everything
-function render(ctx, player1, player2, canvasWidth, canvasHeight) {
-    ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+    // Create the canvas
+    createCanvas() {
+        var canvas = document.createElement('canvas');
+        canvas.width = this.canvasWidth * this.zoom;
+        canvas.height = this.canvasHeight * this.zoom;
+        document.body.appendChild(canvas);
+        return canvas.getContext('2d');
+    }
 
-    // Player 1
-    renderPlayer(ctx, player1);
-    renderPlayer(ctx, player2);
+    main() {
+        var now = Date.now();
+        var dt = (now - this.lastTime) / 1000.0;
+        this.fps = Math.ceil(1000 / (now - this.lastTime));
+        this.update();
+        this.render();
 
-    // Infos debug
-    var text  = 'FPS:' + fps + ' - action:' + player1.action ;
+        this.lastTime = now;
+        requestAnimFrame(this.main.bind(this));
+    }
 
-    ctx.fillStyle = '#000';
-    ctx.font = '10px  Lucida Console';
-    ctx.textBaseline = 'bottom';
-    ctx.fillText(text, 10, 20);
-}
+    init() {
+        this.ctx = this.createCanvas();
+        this.reset();
+        this.lastTime = Date.now();
+        this.main();
+    }
 
-function renderPlayer(ctx, player) {
-    ctx.save();
-    ctx.scale(player.right, 1);
+    // Update game objects
+    update(dt) {
+        //handleInput(dt);
+        //checkCollisions();
+    }
 
-    var groupNumber = (
-        player.AIR[player.action].elements[player.currentFrame].groupNumber
-    );
-    var imageNumber = (
-        player.AIR[player.action].elements[player.currentFrame].imageNumber
-    );
-    var i = player.indexOf(groupNumber, imageNumber);
+    // Draw everything
+    render() {
+        this.ctx.clearRect(0, 0, this.canvasWidth, this.canvasHeight);
 
-    var image = resource.decodePCX(player.SFF.images[i].image, player.palette);
-    var width = player.right === 1 ? 0 : image.width;
+        // Player 1
+        this.renderPlayer(this.player1);
+        this.renderPlayer(this.player2);
 
-    // Fill image
-    ctx.drawImage(
-        image,
-        (
-            (player.right * (player.pos.x - player.SFF.images[i].x))
-            + (player.right * width)
-        ),
-        player.pos.y - player.SFF.images[i].y
-    );
+        // Infos debug
+        var text = 'FPS:' + this.fps + ' - action:' + this.player1.action;
 
-    /*
-    // Fill collision box // TODO Wrong in scale
-    if (player.AIR[player.action].clsn2Default) {
-        var clsn = player.AIR[action].clsn2Default;
-    } else if (player.AIR[player.action].elements[player.currentFrame].clsn2) {
-        var clsn = (
-            player.AIR[player.action].elements[player.currentFrame].clsn2
+        this.ctx.fillStyle = '#000';
+        this.ctx.font = '10px  Lucida Console';
+        this.ctx.textBaseline = 'bottom';
+        this.ctx.fillText(text, 10, 20);
+    }
+
+    renderPlayer(player) {
+        this.ctx.save();
+        this.ctx.scale(player.right, 1);
+
+        var groupNumber = (
+            player.AIR[player.action].elements[player.currentFrame].groupNumber
         );
-    }
-    if (clsn) {
-        for (i = 0; i < clsn.length; i++) {
-            var x = player.pos.x + clsn[i].x;
-            var y = player.pos.y + clsn[i].y ;
-            var width = clsn[i].x2 - clsn[i].x;
-            var height = clsn[i].y2 - clsn[i].y;
-            ctx.fillStyle = 'rgba(0,0,255,0.2)';
-            ctx.fillRect(player.right * x, y, player.right * width, height);
+        var imageNumber = (
+            player.AIR[player.action].elements[player.currentFrame].imageNumber
+        );
+        var i = player.indexOf(groupNumber, imageNumber);
+
+        var image = resource.decodePCX(
+            player.SFF.images[i].image,
+            player.palette
+        );
+        var width = player.right === 1 ? 0 : image.width;
+
+        // Fill image
+        this.ctx.drawImage(
+            image,
+            (
+                (player.right * (player.pos.x - player.SFF.images[i].x))
+                + (player.right * width)
+            ),
+            player.pos.y - player.SFF.images[i].y
+        );
+
+        /*
+        // Fill collision box // TODO Wrong in scale
+        if (player.AIR[player.action].clsn2Default) {
+            var clsn = player.AIR[action].clsn2Default;
+        } else if (
+            player.AIR[player.action].elements[player.currentFrame].clsn2
+        ) {
+            var clsn = (
+                player.AIR[player.action].elements[player.currentFrame].clsn2
+            );
         }
-    }
-    */
-
-    // Fill pos
-    /*
-    ctx.fillStyle = '#ff0000';
-    ctx.fillRect(player.right * player.pos.x, player.pos.y, 1, 1);
-    */
-
-    player.currentTime++;
-    if (
-        player.currentTime
-        >= player.AIR[player.action].elements[player.currentFrame].time
-    ) {
-        player.currentTime = 0;
-        player.currentFrame++;
-        if (player.currentFrame >= player.AIR[player.action].elements.length) {
-            player.currentFrame = 0;
+        if (clsn) {
+            for (i = 0; i < clsn.length; i++) {
+                var x = player.pos.x + clsn[i].x;
+                var y = player.pos.y + clsn[i].y ;
+                var width = clsn[i].x2 - clsn[i].x;
+                var height = clsn[i].y2 - clsn[i].y;
+                ctx.fillStyle = 'rgba(0,0,255,0.2)';
+                ctx.fillRect(player.right * x, y, player.right * width, height);
+            }
         }
+        */
+
+        // Fill pos
+        /*
+        ctx.fillStyle = '#ff0000';
+        ctx.fillRect(player.right * player.pos.x, player.pos.y, 1, 1);
+        */
+
+        player.currentTime++;
+        if (
+            player.currentTime
+            >= player.AIR[player.action].elements[player.currentFrame].time
+        ) {
+            player.currentTime = 0;
+            player.currentFrame++;
+            if (
+                player.currentFrame >= player.AIR[player.action].elements.length
+            ) {
+                player.currentFrame = 0;
+            }
+        }
+
+        this.ctx.restore();
     }
 
-    ctx.restore();
-}
+    // Reset game to original state
+    reset() {
+        this.isGameOver = false;
+        this.gameTime = 0;
 
-// Reset game to original state
-function reset(ctx, zoom) {
-    isGameOver = false;
-    gameTime = 0;
-    score = 0;
+        this.score = 0;
 
-    ctx.scale(zoom || 1, zoom || 1);
+        this.ctx.scale(this.zoom || 1, this.zoom || 1);
+    }
 }
 
 module.exports = {
-    init: init,
-    loadCharacters: loadCharacters
+    MugenJSApp: MugenJSApp
 };

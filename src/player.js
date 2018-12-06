9d983cc8f5bf8356d1aebad97668eed5657385be
Carlson Santana Cruz <carlsonsantana@gmail.com>
2018-12-05 22:30:30 -0300

    :art: Replace tabs by four spaces

--- src/player.js
+++ src/player.js
@@ -1,36 +1,36 @@
 
 function player ( resource ) {
-			
-	this.pos = { x : 0, y : 0 };
-	this.action = 0;
-	this.currentFrame = 0;
-	this.currentTime = 0;
-	this.right = 1;
-	this.palette = null;
-	
-	var hasOwn = Object.prototype.hasOwnProperty;
-	if ( typeof resource != 'object' ) {
-		throw TypeError( 'player - ressource incorrect' );
-	}
-	var properties = Object( resource );
-	for ( var prop in properties ) {
-		if ( hasOwn.call( properties, prop ) ) {
-			this[ prop ] = properties[ prop ];
-		}
-	}
-	
+            
+    this.pos = { x : 0, y : 0 };
+    this.action = 0;
+    this.currentFrame = 0;
+    this.currentTime = 0;
+    this.right = 1;
+    this.palette = null;
+    
+    var hasOwn = Object.prototype.hasOwnProperty;
+    if ( typeof resource != 'object' ) {
+        throw TypeError( 'player - ressource incorrect' );
+    }
+    var properties = Object( resource );
+    for ( var prop in properties ) {
+        if ( hasOwn.call( properties, prop ) ) {
+            this[ prop ] = properties[ prop ];
+        }
+    }
+    
 }
 
 player.prototype = {
-	indexOf : function( groupNumber, imageNumber ) {
-		if ( this.SFF == null ) {
-			throw new TypeError( 'indexOf - SFF not defined.' );
-		}
-		for ( var i = 0; i < this.SFF.images.length; i++ ) {
-			if ( this.SFF.images[ i ].groupNumber === groupNumber && this.SFF.images[ i ].imageNumber === imageNumber ) {
-				return i;
-			}
-		}
-		return -1;
-	}
+    indexOf : function( groupNumber, imageNumber ) {
+        if ( this.SFF == null ) {
+            throw new TypeError( 'indexOf - SFF not defined.' );
+        }
+        for ( var i = 0; i < this.SFF.images.length; i++ ) {
+            if ( this.SFF.images[ i ].groupNumber === groupNumber && this.SFF.images[ i ].imageNumber === imageNumber ) {
+                return i;
+            }
+        }
+        return -1;
+    }
 }

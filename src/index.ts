495e7ed644a74f16b4956a75f9cafefa559f1662
Carlson Santana Cruz <carlsonsantana@gmail.com>
2021-02-27 00:32:50 -0300

    Compile project using typescript compiler

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import {Resource as ResourceImport} from './resource';
+import {Player as PlayerImport} from './player';
+import {RoundOneJSApp as RoundOneJSAppImport} from './app';
+
+export const Resource = ResourceImport;
+export const Player = PlayerImport;
+export const RoundOndeJSApp = RoundOneJSAppImport;
